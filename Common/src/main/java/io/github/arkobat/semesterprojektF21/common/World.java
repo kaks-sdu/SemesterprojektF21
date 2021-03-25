33adb6f12f7e6afa4e878ce79374a76fc477d296
Arkobat <kr-jo@live.dk>
2021-03-25 10:16:30 +0100

    Added comments

--- Common/src/main/java/io/github/arkobat/semesterprojektF21/common/World.java
+++ Common/src/main/java/io/github/arkobat/semesterprojektF21/common/World.java
@@ -7,21 +7,32 @@ import java.util.Collection;
 public interface World {
 
     /**
-     * Gets all entities.
-     * @return Collection<Entity> - a collection of all entities
+     * Get all entities in the world
+     *
+     * @return a collection on entities
      */
     Collection<Entity> getEntities();
+
+    /**
+     * Get all of specific entities in the world
+     *
+     * @param entityTypes Filter of which types wanted
+     * @param <E> Filter of which types wanted
+     * @return a filtered collection on entities
+     */
     <E extends Entity> Collection<Entity> getEntities(Class<E>... entityTypes);
 
 
     /**
      * Adds an entity.
+     *
      * @param entity - an entity has a location and a vector for velocity.
      */
     void addEntity(Entity entity);
 
     /**
      * Removes an entity.
+     *
      * @param entity - an entity has a location and a vector for velocity.
      */
     void removeEntity(Entity entity);

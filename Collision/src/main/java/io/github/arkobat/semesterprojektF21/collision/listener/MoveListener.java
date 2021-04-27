a2d17891cda30d5f32cedb55d66f068b7b82f811
Arkobat <kr-jo@live.dk>
2021-04-27 16:45:02 +0200

    Fixed small collisions bug

--- Collision/src/main/java/io/github/arkobat/semesterprojektF21/collision/listener/MoveListener.java
+++ Collision/src/main/java/io/github/arkobat/semesterprojektF21/collision/listener/MoveListener.java
@@ -14,6 +14,104 @@ import io.github.arkobat.semesterprojektF21.commonWorld.WorldTemp;
 
 public class MoveListener extends EventListener {
 
+
+    @Override
+    public void onEntityMove(EntityMoveEvent event) {
+        if (!event.getEntity().hasCollision()) {
+            return;
+        }
+
+        World world = event.getEntity().getWorld();
+        if (!(world instanceof WorldTemp)) {
+            return;
+        }
+
+        final TiledMapTileLayer collisionLayer = ((WorldTemp) world).getCollisionLayer();
+
+        Entity entity = event.getEntity();
+        Location loc = entity.getLocation();
+        Hitbox hitbox = entity.getHitbox();
+
+
+        // Ensure the player is within game borders
+        if (loc.getX() + hitbox.getOffsetX() < 0) {
+            loc.setX(0);
+        } else if (loc.getX() + hitbox.getWidth() + hitbox.getOffsetX() > Gdx.graphics.getWidth()) {
+            loc.setX(collisionLayer.getWidth() * collisionLayer.getTileWidth() - hitbox.getWidth());
+        }
+        if (loc.getY() + hitbox.getOffsetY() < 0) {
+            loc.setY(0);
+        } else if (loc.getY() + hitbox.getHeight() + hitbox.getOffsetY() > collisionLayer.getHeight() * collisionLayer.getTileHeight()) {
+            loc.setY(collisionLayer.getHeight() * collisionLayer.getTileHeight() - hitbox.getHeight());
+        }
+
+        boolean blocked = false;
+
+        if (entity.getVelocity().getX() > 0) {
+            int cellX = (int) (loc.getX() + hitbox.getWidth()) / collisionLayer.getTileWidth();
+            int cellY = (int) (loc.getY() + 0.1) / collisionLayer.getTileHeight();
+            while (cellY * collisionLayer.getTileHeight() < loc.getY() + hitbox.getHeight()) {
+                if (checkCollision(collisionLayer, event.getEntity(), cellX, cellY)) {
+                    blocked = true;
+                    break;
+                }
+                cellY++;
+            }
+
+        } else if (entity.getVelocity().getX() < 0) {
+            int cellX = (int) loc.getX() / collisionLayer.getTileWidth();
+            int cellY = (int) (loc.getY() + 0.1) / collisionLayer.getTileHeight();
+            while (cellY * collisionLayer.getTileHeight() < loc.getY() + hitbox.getHeight()) {
+                if (checkCollision(collisionLayer, event.getEntity(), cellX, cellY)) {
+                    blocked = true;
+                    break;
+                }
+                cellY++;
+            }
+        }
+
+        if (blocked) {
+            loc.setX(event.getOldLocation().getX());
+            entity.getVelocity().setX(0);
+            blocked = false;
+        }
+
+        // Check collision Y
+        if (entity.getVelocity().getY() > 0) {
+            int cellX = (int) loc.getX() / collisionLayer.getTileWidth();
+            int cellY = (int) (loc.getY() + hitbox.getHeight()) / collisionLayer.getTileHeight();
+            while (cellX * collisionLayer.getTileWidth() < loc.getX() + hitbox.getWidth()) {
+                if (checkCollision(collisionLayer, event.getEntity(), cellX, cellY)) {
+                    blocked = true;
+                    break;
+                }
+                cellX++;
+            }
+
+        } else if (entity.getVelocity().getY() < 0) {
+            int cellX = (int) loc.getX() / collisionLayer.getTileWidth();
+            int cellY = (int) loc.getY() / collisionLayer.getTileHeight();
+            while (cellX * collisionLayer.getTileWidth() < loc.getX() + hitbox.getWidth()) {
+                if (checkCollision(collisionLayer, event.getEntity(), cellX, cellY)) {
+                    blocked = true;
+                    if (entity instanceof Player) {
+                        ((Player) entity).setJumpCharges(2);
+                    }
+                    break;
+                }
+                cellX++;
+            }
+        }
+
+        if (blocked) {
+            entity.getVelocity().setY(0);
+            loc.setY(event.getOldLocation().getY());
+        }
+
+
+    }
+
+    /*
     @Override
     public void onEntityMove(EntityMoveEvent event) {
         if (!event.getEntity().hasCollision()) {
@@ -88,7 +186,7 @@ public class MoveListener extends EventListener {
             }
         }
     }
-
+*/
 
     private boolean checkCollision(TiledMapTileLayer collisionLayer, Colorable colorable, int x, int y) {
         TiledMapTileLayer.Cell cell = collisionLayer.getCell(x, y);

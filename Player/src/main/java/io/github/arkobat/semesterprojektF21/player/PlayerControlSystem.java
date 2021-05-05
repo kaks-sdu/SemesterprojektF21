8711b64ac9e08eecfb44c7dd7dad42418f3572fd
Svendpai <svvib19@student.sdu.dk>
2021-05-05 11:23:41 +0200

    updated and added more maps

--- Player/src/main/java/io/github/arkobat/semesterprojektF21/player/PlayerControlSystem.java
+++ Player/src/main/java/io/github/arkobat/semesterprojektF21/player/PlayerControlSystem.java
@@ -22,7 +22,7 @@ public class PlayerControlSystem implements GameProcessingService, TextureRender
 
     private static final float acceleration = 150F;
     private static final float deacceleration = 250F;
-    private static final float jumpAcceleration = 75F;
+    private static final float jumpAcceleration = 80F;
     private static final float gravity = 250F;
     private static final float maxAcceleration = 75F;
 

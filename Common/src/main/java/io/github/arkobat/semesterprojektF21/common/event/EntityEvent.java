ede865ef190aa74657ad58418df2620a212f892d
Svendpai <svvib19@student.sdu.dk>
2021-05-18 12:33:09 +0200

    added comments to event classes

--- Common/src/main/java/io/github/arkobat/semesterprojektF21/common/event/EntityEvent.java
+++ Common/src/main/java/io/github/arkobat/semesterprojektF21/common/event/EntityEvent.java
@@ -5,14 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
-public class EntityEvent extends Event {
+/**
+ * An abstract event class for handling an Entity
+ */
+public abstract class EntityEvent extends Event {
 
     private final @NotNull Entity entity;
 
+    /**
+     * A constructor taking in an Entity object
+     * @param entity requires an Entity object
+     */
     public EntityEvent(@NotNull Entity entity) {
         this.entity = entity;
     }
 
+    /**
+     * A method for getting the entity field
+     * @return an entity
+     */
     public @NotNull Entity getEntity() {
         return entity;
     }
--- Common/src/main/java/io/github/arkobat/semesterprojektF21/common/event/Event.java
+++ Common/src/main/java/io/github/arkobat/semesterprojektF21/common/event/Event.java
@@ -2,6 +2,9 @@ package io.github.arkobat.semesterprojektF21.common.event;
 
 import java.io.Serializable;
 
+/**
+ * An abstract definition of an Event
+ */
 public abstract class Event implements Serializable {
 
 }

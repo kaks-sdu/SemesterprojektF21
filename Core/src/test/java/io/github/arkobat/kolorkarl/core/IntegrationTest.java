5490af1bf65d91aed22c910a8e902c16addf1341
khave <kristian.have@hotmail.com>
2021-05-21 18:49:27 +0200

    Integration tests and other tests

--- Core/src/test/java/io/github/arkobat/kolorkarl/core/IntegrationTest.java
+++ Core/src/test/java/io/github/arkobat/kolorkarl/core/IntegrationTest.java
@@ -1,58 +1,73 @@
 package io.github.arkobat.kolorkarl.core;
 
 import org.apache.felix.framework.FrameworkFactory;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.launch.Framework;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
 import java.io.File;
-import java.net.URL;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.ServiceLoader;
+import java.util.Map;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
+
 
 public class IntegrationTest {
 
+    static BundleContext context;
+
+    @BeforeClass
+    public static void setup() {
+        // Start the framework
+        FrameworkFactory frameworkFactory = new FrameworkFactory();
+
+        Map<String, String> config = new HashMap<String, String>();
+        config.put("osgi.console", "");
+        config.put("osgi.clean", "true");
+        config.put("osgi.noShutdown", "true");
+        config.put("eclipse.ignoreApp", "true");
+        config.put("osgi.bundles.defaultStartLevel", "4");
+        config.put("osgi.configuration.area", "./configuration");
+
+        // automated bundles deployment
+        config.put("felix.fileinstall.dir", "./dropins");
+        config.put("felix.fileinstall.noInitialDelay", "true");
+        config.put("felix.fileinstall.start.level", "4");
+
+        Framework framework = frameworkFactory.newFramework(config);
+
+        try {
+            framework.start();
+        } catch (BundleException e) {
+            e.printStackTrace();
+        }
+
+        context = framework.getBundleContext();
+    }
+
     @Test
-    public void integrationTest() throws BundleException {
-        FrameworkFactory ffFactory = new FrameworkFactory();
-        Framework ff = ffFactory.newFramework(new HashMap<String, String>());
-        ff.start();
-
-        System.out.println("Bundles pre installed:");
-        for(Bundle bundle : ff.getBundleContext().getBundles()){
-            System.out.println(bundle.getSymbolicName());
+    public void installBundle_AssetManagerInstalled_newFramework() {
+        try {
+            installBundle("SemesterProjectGroup1_AssetManager_1.0.0.SNAPSHOT.jar");
+        } catch (BundleException e) {
+            fail("Could not install AssetManager bundle");
         }
-        System.out.println("===============================");
+    }
 
-        try
-        {
-            // Bundle 0 is the org.apache.felix.framework bundle
-            final Bundle b = ff.getBundleContext ().getBundle (0);
+    @Test
+    public void installBundle_unknownBundle_newFramework() {
+        assertThrows(BundleException.class, () -> installBundle("unknown_bundle.jar"));
+    }
 
-            assertNotNull (b);
-            assertEquals (Bundle.ACTIVE, b.getState ());
+    public void installBundle(String jar) throws BundleException {
+        String jarUrl = Paths.get(new File("").getAbsolutePath(), "runner", "bundles", jar).toString();
+        jarUrl = jarUrl.replace("Core", "");
 
-            // No leading slash is important as the ClassLoader is used!
-            assertNotNull (b.getResource ("org/apache/felix/framework/util/Util.class"));
-            // Assert not throws bundle exception. If this is thrown, the bundle cannot be found.
-            String jarUrl = Paths.get(new File("").getAbsolutePath()).toString();
-            jarUrl = jarUrl.replace("Core", "runner\\bundles\\SemesterProjectGroup1_Player_1.0.0.SNAPSHOT.jar").replace('\\', '/');
-            System.out.println("Jar URL:");
-            System.out.println("file:///" + jarUrl);
-
-            Bundle playerBundle = ff.getBundleContext().installBundle("file://" + jarUrl);
-            System.out.println("Player bundle: " + playerBundle.getSymbolicName());
-            //assertNotNull(b.getResource("reference:file:../runner/bundles/SemesterProjectGroup1_Player_1.0.0.SNAPSHOT.jar"));
-        }
-        finally
-        {
-            ff.stop ();
-            System.out.println("Test done!");
-        }
+        context.installBundle("file:" + jarUrl);
     }
 }
---
+++ Player/src/test/java/io/github/arkobat/kolorkarl/player/PlayerControlSystemTest.java
@@ -0,0 +1,53 @@
+package io.github.arkobat.kolorkarl.player;
+import io.github.arkobat.kolorkarl.common.Color;
+import io.github.arkobat.kolorkarl.common.Location;
+import io.github.arkobat.kolorkarl.common.Vector;
+import io.github.arkobat.kolorkarl.common.World;
+import io.github.arkobat.kolorkarl.common.entity.Entity;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PlayerControlSystemTest {
+
+    @Mock
+    private World world;
+
+    private Entity player;
+
+    @Before
+    public void setup(){
+        player = new PlayerImpl(world, new Color[]{ Color.BLUE, Color.GREEN, Color.ORANGE }, new Location(20, 20));
+    }
+
+    @After
+    public void tearDown(){
+        player = null;
+    }
+
+    // method, expected, current condition
+    @Test
+    public void getLocation_x20y20_spawned() {
+        // Test player has spawned at correct location
+        assertEquals(20, player.getLocation().getX(), 0.001);
+        assertEquals(20, player.getLocation().getY(), 0.001);
+    }
+
+    @Test
+    public void getLocation_xOver20_velocity20(){
+        // Make player move
+        Vector velocity = player.getVelocity();
+
+        long start = System.currentTimeMillis();
+        long end = start + 1000;
+        while(System.currentTimeMillis() < end){
+            velocity.setX(20);
+        }
+        assertTrue(player.getLocation().getX() > 20);
+    }
+}
